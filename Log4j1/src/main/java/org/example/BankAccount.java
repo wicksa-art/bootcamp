c163264618bfda73e7030be11d75bbcbf9124446
Pavel Kleymenov <kleymennno@outlook.lv>
2023-08-07 15:01:02 +0300

    Bank JUnit/log4j2 task done

--- Log4j1/src/main/java/org/example/BankAccount.java
+++ Log4j1/src/main/java/org/example/BankAccount.java
@@ -2,5 +2,34 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class BankAccount {
-    
+
+    private double balance;
+    private static final Logger logger = LogManager.getLogger(BankAccount.class);
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+        logger.info("New account created with balance: " + initialBalance);
+    }
+
+    public void deposit(double amount) {
+        if (amount < 0) {
+            logger.error("Cannot deposit negative amounts.");
+            throw new IllegalArgumentException("Cannot deposit negative amounts.");
+        }
+        this.balance += amount;
+        logger.info("Deposited " + amount + ". Current balance: " + balance);
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            logger.error("Cannot withdraw more than the current balance.");
+            throw new IllegalArgumentException("Cannot withdraw more than the current balance.");
+        }
+        this.balance -= amount;
+        logger.info("Withdrew " + amount + ". Current balance: " + balance);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
--- Log4j1/src/test/java/org/example/BankAccountTest.java
+++ Log4j1/src/test/java/org/example/BankAccountTest.java
@@ -1,9 +1,48 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class BankAccountTest {
-    BankAccount account;
+public class BankAccountTest {
 
+    private BankAccount account;
+    private static final Logger logger = LogManager.getLogger(BankAccountTest.class);
 
+    @BeforeEach
+    public void setup() {
+        account = new BankAccount(100);
+        logger.info("Account setup with balance: " + account.getBalance());
+    }
+
+    @Test
+    public void testDeposit() {
+        account.deposit(50);
+        logger.info("Deposited 50. Balance: " + account.getBalance());
+        assertEquals(150, account.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() {
+        account.withdraw(50);
+        logger.info("Withdrew 50. Balance: " + account.getBalance());
+        assertEquals(50, account.getBalance());
+    }
+
+    @Test
+    public void testDepositNegativeAmount() {
+        logger.info("Trying to deposit negative amount.");
+        assertThrows(IllegalArgumentException.class, () -> {
+            account.deposit(-50);
+        });
+    }
+
+    @Test
+    public void testWithdrawMoreThanBalance() {
+        logger.info("Trying to withdraw more than the balance.");
+        assertThrows(IllegalArgumentException.class, () -> {
+            account.withdraw(200);
+        });
+    }
 }
